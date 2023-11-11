e21a74527c861292df58efbe18518222199598b2
Cisco Vlahakis <ciscovlahakis@gmail.com>
2023-11-11 22:51:42 +0000

    finished

--- app.rb
+++ app.rb
@@ -2,8 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:squareWithForm)
+end
+
+get("/square/new") do
+  erb(:squareWithForm)
+end
+
+get("/square/results") do
+  erb(:squareWithFormResults)
+end
+
+get("/square_root/new") do
+  erb(:squareRoot)
+end
+
+get("/square_root/results") do
+  erb(:squareRootResults)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  erb(:paymentResults)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  erb(:randomResults)
 end
